684d93893dc4f414c1f8af67c5c93ba09c28b6ee
Melissa <melissamichaud0319@gmail.com>
2023-08-09 01:49:26 -0400

    final edits

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,58 @@ import { Link } from "react-router-dom";
 import "./About.css";
 import edelweiss2 from "../../assets/edelweiss2.png";
 
+const BookCard = ({ image, title, author }) => {
+  return (
+    <div className="book-card">
+      <img src={image} alt={title} className="book-card-img" />
+      <h3 className="book-card-title">{title}</h3>
+      <p className="book-card-author">{author}</p>
+    </div>
+  );
+};
+
 const About = () => {
+  const popularBooks = [
+    {
+      image:
+        "https://edel-images-plus.azureedge.net/assets/OO/images/imprint_logos/original/ATL_logo_rgb2.png?format=png",
+      title: "Space Cats",
+      author: "asdf",
+    },
+    {
+      image:
+        "https://edel-images-plus.azureedge.net/assets/OO/images/imprint_logos/original/cheezburger.png?format=png",
+      title: "Supercharged Panda Go-Carts",
+      author: "Tannenbaum, Clara",
+    },
+    {
+      image:
+        "https://edel-images.azureedge.net/ea/OO/images/jacket_covers/original/1000000007_5bbee.jpg",
+      title: "stuff - take 2: New Subtitle",
+      author: "Uncle, Bob",
+    },
+    {
+      image:
+        "https://edel-images.azureedge.net/ea/OO/images/jacket_covers/original/2000000003_e5f6c.jpg",
+      title: "Who ate the Walrus?",
+      author: "Doe, Jane",
+    },
+    {
+      image:
+        "https://edel-images.azureedge.net/ea/OO/images/jacket_covers/original/2000000004_518da.jpeg",
+      title: "Jen's US Trade Title w/ US Trade Stuff!: Test US Subtitle",
+      author: "Slajus, Jen",
+    },
+    {
+      image:
+        "https://edel-images.azureedge.net/ea/OO/images/jacket_covers/original/5000000999_9f56b.jpg",
+      title: "Candy Crush Saga-Jason's Story",
+      author: "Nesler, NotMax",
+    },
+
+    // Add more book objects as needed
+  ];
+
   return (
     <section className="about">
       <div className="container">
@@ -18,14 +69,41 @@ const About = () => {
             <img src={edelweiss2} alt="" />
           </div>
           <div className="about-text">
-            <h2 className="about-title fs-26 ls-1">Workflow</h2>
-            <p className="fs-15">Explain</p>
+            <h2 className="about-title fs-26 ls-1">About Us</h2>
             <p className="fs-15">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-              dicta, possimus inventore eveniet atque voluptatibus repellendus
-              aspernatur illo aliquam dignissimos illum. Commodi, porro omnis
-              dolore amet neque modi quas eum!
+              Welcome to our cozy corner of the internet, where the joy of
+              reading knows no bounds. 📖✨ Our website is your go-to hub for
+              all things books and authors, bringing together book enthusiasts
+              from far and wide. 🌟📚
             </p>
+            <p className="fs-15">
+              Embark on a seamless journey to find the books and authors that
+              resonate with your soul. Our user-friendly search feature allows
+              you to explore a world of literary wonders with a simple click.
+              🕵️‍♀️🔍
+              <p className="fs-15">
+                Whether you're a casual reader or an avid book collector, our
+                website invites you to embark on a journey of literary
+                exploration and connection. 🚀📚 Let's turn the pages of
+                imagination together and create a story that celebrates the
+                magic of books! 🌈✨
+              </p>
+            </p>
+          </div>
+          <div className="about-text">
+            <h2 className="about-title fs-26 ls-1">
+              Here are a few examples of the books in our catalog!
+            </h2>
+          </div>
+          <div className="book-cards">
+            {popularBooks.map((book, index) => (
+              <BookCard
+                key={index}
+                image={book.image}
+                title={book.title}
+                author={book.author}
+              />
+            ))}
           </div>
         </div>
       </div>
